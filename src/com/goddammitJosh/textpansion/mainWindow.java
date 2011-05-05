bc0d44a76dec6847354a4bf6b2edfdf8e6910f9d
Sean Barag sjb89@drexel.edu <Sean Barag sjb89@drexel.edu>
2011-05-05 15:49:32 -0400

    Swapped order of tabs when opening from launcher.

--- src/com/goddammitJosh/textpansion/mainWindow.java
+++ src/com/goddammitJosh/textpansion/mainWindow.java
@@ -21,18 +21,19 @@ public class mainWindow extends TabActivity
         Intent intent;                  // reusable intent for each tab
 
         // create an intent to launch an activity for the tab (to be reused)
-        intent = new Intent().setClass(this, settings.class);
+        intent = new Intent().setClass(this, subsList.class);
 
         // --- TODO: make the text for each tab a reference to strings.xml ---
 
         // initialize a tabspec for each tab and add it to the tabhost
-        spec = tabHost.newTabSpec("settings").setIndicator("Settings",
+        spec = tabHost.newTabSpec("subs").setIndicator("Substitutions",
             res.getDrawable(R.drawable.ic_tab_settings)).setContent(intent);
         tabHost.addTab(spec);
 
+
         // do the same for the others!
-        intent = new Intent().setClass(this, subsList.class);
-        spec = tabHost.newTabSpec("subs").setIndicator("Substitutions",
+        intent = new Intent().setClass(this, settings.class);
+        spec = tabHost.newTabSpec("settings").setIndicator("Settings",
             res.getDrawable(R.drawable.ic_tab_settings)).setContent(intent);
         tabHost.addTab(spec);
 
