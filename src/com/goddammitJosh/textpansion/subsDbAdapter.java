00e70d2c31dd8aad1711fc6368898b2a9363b356
Sean Barag sjb89@drexel.edu <Sean Barag sjb89@drexel.edu>
2011-05-06 18:25:50 -0400

    Beginnings of SQLite support.
    Right now, this does not display anything in each row.  However, there is
    persistence between sessions and even between reboots.  This is promising!

---
+++ src/com/goddammitJosh/textpansion/subsDbAdapter.java
@@ -0,0 +1,185 @@
+package com.goddammitJosh.textpansion;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Simple substitutions database access helper class. Defines the basic CRUD
+ * operations for textspansion's substituions, and gives the ability to list
+ * all subs as well as retrieve or modify a specific substitution.
+ * 
+ * This has been improved from the first version of this tutorial through the
+ * addition of better error handling and also using returning a Cursor instead
+ * of using a collection of inner classes (which is less scalable and not
+ * recommended).
+ */
+public class subsDbAdapter 
+{
+
+    public static final String KEY_ABBR = "abbr"; // short name of the sub
+    public static final String KEY_FULL = "full"; // full sub
+    public static final String KEY_ROWID = "_id"; // row's id
+
+    private static final String TAG = "Textspansion: subsDbAdapter";
+    private DatabaseHelper mDbHelper;
+    private SQLiteDatabase mDb;
+
+    /**
+     * Database creation sql statement
+     */
+    private static final String DATABASE_CREATE =
+        "create table subs (_id integer primary key autoincrement, "
+        + "abbr text not null, full text not null);";
+
+    private static final String DATABASE_NAME = "data";
+    private static final String DATABASE_TABLE = "subs";
+    private static final int DATABASE_VERSION = 2;
+
+    private final Context mCtx;
+
+    private static class DatabaseHelper extends SQLiteOpenHelper
+    {
+
+        DatabaseHelper(Context context) 
+        {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db)
+        {
+
+            db.execSQL(DATABASE_CREATE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+        {
+            Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
+                    + newVersion + ", which will destroy all old data");
+            db.execSQL("DROP TABLE IF EXISTS subs");
+            onCreate(db);
+        }
+    }
+
+    /**
+     * Constructor - takes the context to allow the database to be
+     * opened/created
+     * 
+     * @param ctx the Context within which to work
+     */
+    public subsDbAdapter(Context ctx)
+    {
+        this.mCtx = ctx;
+    }
+
+    /**
+     * Open the subs database. If it cannot be opened, try to create a new
+     * instance of the database. If it cannot be created, throw an exception to
+     * signal the failure
+     * 
+     * @return this (self reference, allowing this to be chained in an
+     *         initialization call)
+     * @throws SQLException if the database could be neither opened or created
+     */
+    public subsDbAdapter open() throws SQLException
+    {
+        mDbHelper = new DatabaseHelper(mCtx);
+        mDb = mDbHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close()
+    {
+        mDbHelper.close();
+    }
+
+
+    /**
+     * Create a new substitution using the abbreviated and full names provided.
+     * If the substitution is successfully created return the new rowId for
+     * that sub, otherwise return a -1 to indicate failure.
+     * 
+     * @param abbr the abbreviated version of the substitution
+     * @param full the full version of the substitution
+     * @return rowId or -1 if failed
+     */
+    public long createSub(String abbr, String full)
+    {
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(KEY_ABBR, abbr);
+        initialValues.put(KEY_FULL, full);
+
+        return mDb.insert(DATABASE_TABLE, null, initialValues);
+    }
+
+    /**
+     * Delete the sub with the given rowId
+     * 
+     * @param rowId id of sub to delete
+     * @return true if deleted, false otherwise
+     */
+    public boolean deleteSub(long rowId)
+    {
+
+        return mDb.delete(DATABASE_TABLE, KEY_ROWID + "=" + rowId, null) > 0;
+    }
+
+    /**
+     * Return a Cursor over the list of all substitutions in the database
+     * 
+     * @return Cursor over all subs
+     */
+    public Cursor fetchAllSubs()
+    {
+        return mDb.query(DATABASE_TABLE, new String[] {KEY_ROWID, KEY_ABBR,
+                KEY_FULL}, null, null, null, null, null);
+    }
+
+    /**
+     * Return a Cursor positioned at the substitution that matches the given rowId
+     * 
+     * @param rowId id of sub to retrieve
+     * @return Cursor positioned to matching sub, if found
+     * @throws SQLException if sub could not be found/retrieved
+     */
+    public Cursor fetchSub(long rowId) throws SQLException
+    {
+
+        Cursor mCursor =
+
+            mDb.query(true, DATABASE_TABLE, new String[] {KEY_ROWID,
+                    KEY_ABBR, KEY_FULL}, KEY_ROWID + "=" + rowId, null,
+                    null, null, null, null);
+        if (mCursor != null)
+        {
+            mCursor.moveToFirst();
+        }
+        return mCursor;
+
+    }
+
+    /**
+     * Update the sub using the details provided. The sub to be updated is
+     * specified using the rowId, and it is altered to use the abbreviated and
+     * full values passed in
+     * 
+     * @param rowId id of sub to update
+     * @param abbr value to which the sub's abbreviation will be set
+     * @param full value to which the sub's full text will be set
+     * @return true if the sub was successfully updated, false otherwise
+     */
+    public boolean updateSub(long rowId, String abbr, String full)
+    {
+        ContentValues args = new ContentValues();
+        args.put(KEY_ABBR, abbr);
+        args.put(KEY_FULL, full);
+
+        return mDb.update(DATABASE_TABLE, args, KEY_ROWID + "=" + rowId, null) > 0;
+    }
+}
--- src/com/goddammitJosh/textpansion/subsList.java
+++ src/com/goddammitJosh/textpansion/subsList.java
@@ -38,104 +38,106 @@ import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.AdapterView.AdapterContextMenuInfo;
 
 //sorted map
-import java.util.SortedMap;
+/*import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Iterator;*/
+
+import android.widget.SimpleCursorAdapter;
+import android.database.Cursor;
 
 public class subsList extends ListActivity //implements OnGlobalFocusChangeListener
 {
+    public static final int INSERT_ID = Menu.FIRST;
 
-    ArrayAdapter<String> aa; 
-    SortedMap subsMap;
-    List<String> subs;
-	
+    private int mSubNumber = 1;
+
+    private subsDbAdapter mDbHelper;
+    private Cursor mSubsCursor;
+
+    //private final ClipboardManager cb = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-		
-        String act = null;
-        try
-        {
-            act = ((Activity) this).getCallingPackage();
-        }
-        catch (NullPointerException ex)
-        {
-            Log.i("sublist", "WTF");
-        }
-        finally
-        {
-            if (act==null)
-                Log.i("sublist", "act is null");
-            else
-                Log.i("subsList", act);
-        }
-    
-        final ClipboardManager cb = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
-
-        String[] substitutions = getResources().getStringArray(R.array.subs_array);
-		
-		//Sets up SortedMap of strings to keep everything organized
-        subsMap = (SortedMap) getLastNonConfigurationInstance();
-        if (subsMap == null)
-        {
-            subsMap = new TreeMap();
-            for(String e : substitutions)
-            {
-                subsMap.put(e, e);
-            }
-        }
-        
-		subs = new ArrayList(subsMap.values());
-		
-		//Initialize arrayadapter
-        aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, subs);
-        setListAdapter(aa);
-
+        setContentView(R.layout.subs_list); // TODO: change to a real list
+        mDbHelper = new subsDbAdapter(this);
+        mDbHelper.open();
+        fillData();
         ListView lv = getListView();
-        lv.setTextFilterEnabled(true);
-
-        // allows each item to have a context menu
         registerForContextMenu(lv);
-
-        lv.setOnItemClickListener(new OnItemClickListener()
-        {
-            public void onItemClick(AdapterView<?> parent, View view,
-                int position, long id)
-            {
-                // when clicked, copy to clipboard!
-                //ClipboardManager cb =  new ClipboardManager();
-                cb.setText(( (TextView)view).getText() );
-
-                /* this is all for API level 11 (Honeycomb, i believe)
-                 * ClipData.Item item = ClipData.Item(view.getText());
-                 * ClipData text = ClipData( "Expansion", MIMETYPE_TEXT_PLAIN, item);
-                 * cb.setPrimaryClip(text); */
-                
-                // notify that copy was successful!
-                String toastString = ((TextView) view).getText().toString().concat(" ");
-                toastString = toastString.concat(getResources().getString(R.string.toast_copied));
-                
-                Toast.makeText(getApplicationContext(), toastString,
-                    Toast.LENGTH_SHORT).show();
-
-                // return to the app you were in.
-                finish();
-            }
-        });
-    } // onCreate
+    }
 
     @Override
+    protected void onListItemClick(ListView l, View v, int position, long id)
+    {
+        super.onListItemClick(l, v, position, id);
+        Cursor c = mSubsCursor;
+        Log.i("textspansion", "Clicked");
+        Toast.makeText(getApplicationContext(), "Clicked!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        boolean result = super.onCreateOptionsMenu(menu);
+        menu.add(0, INSERT_ID, 0, R.string.menu_add_item);
+        return result;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case INSERT_ID:
+                createSub();
+                return true;
+            // add delete here ?
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void createSub()
+    {
+        String subName = "sub " +mSubNumber++;
+        mDbHelper.createSub(subName, "herp");
+        fillData();
+    }
+
+    private void fillData()
+    {
+        mSubsCursor = mDbHelper.fetchAllSubs();
+        startManagingCursor(mSubsCursor);
+
+        String[] from = new String[] { subsDbAdapter.KEY_ABBR };
+        int[] to = new int[] { R.id.list_item };
+
+        // Now create an array adapter and set it to display using the stock android row
+        SimpleCursorAdapter subsAdapter = new SimpleCursorAdapter(this,
+            android.R.layout.simple_list_item_1, mSubsCursor, from, to);
+        setListAdapter(subsAdapter);
+    }
+
+        
+
+
+
+		
+    
+
+/*    @Override
     public Object onRetainNonConfigurationInstance()
     {
         SortedMap savedMap = new TreeMap();
         savedMap.putAll(subsMap);
         return savedMap;
-    }   
+    }   */
 
-    @Override
+/*    @Override // ours!
     public boolean onCreateOptionsMenu(Menu menu)
     {
         MenuInflater inflater = getMenuInflater();
@@ -143,22 +145,23 @@ public class subsList extends ListActivity //implements OnGlobalFocusChangeListe
         return true;
     }
 
-    @Override
+    @Override // ours!
     public boolean onOptionsItemSelected(MenuItem item)
     {
         //Handle item selection
         switch (item.getItemId())
         {
             case R.id.add_item:
-                addItem();
+                createSub();
+                
                 return true;
-            case R.id.delete:
-                deleteItems();
-                return true;
+            //case R.id.delete:
+            //    deleteItems();
+            //    return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
-    }
+    }*/
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo)
@@ -175,17 +178,17 @@ public class subsList extends ListActivity //implements OnGlobalFocusChangeListe
         switch (item.getItemId()) // can get replaced by android:onClick="method name" in sub_context_menu.xml?
         {
             case R.id.edit_item:
-                editItem(info.id);
+                //editItem(info.id);
                 return true;
             case R.id.delete_item:
-                deleteItem(subsMap, info.id);
+                //deleteItem(subsMap, info.id);
                 return true;
             default:
                 return super.onContextItemSelected(item);
         }
     }
 
-    public void addItem()
+/*    public void addItem()
     {
 		final Dialog dialog = new Dialog(subsList.this);
 		dialog.setContentView(R.menu.maindialog);
@@ -219,10 +222,10 @@ public class subsList extends ListActivity //implements OnGlobalFocusChangeListe
 		});
 		dialog.show();
 
-    }
+    }*/
 	
 
-    public void deleteItems()
+/*    public void deleteItems()
     {
         Toast.makeText(getApplicationContext(), "Multi-delete coming not so soon.", Toast.LENGTH_SHORT).show();
     }
@@ -243,6 +246,6 @@ public class subsList extends ListActivity //implements OnGlobalFocusChangeListe
         subs.clear();
         subs.addAll(map.values());
         aa.notifyDataSetChanged();
-    }
+    }*/
 
 }
