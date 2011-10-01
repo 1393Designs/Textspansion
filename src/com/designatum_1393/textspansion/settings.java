4a1403e590e040555b340878ffb8bed8ae7ea649
Vincent Tran <hello@vincetran.me>
2011-10-01 14:34:41 -0300

    Fixed the About Dialog to reflect new company name.

--- src/com/designatum_1393/textspansion/settings.java
+++ src/com/designatum_1393/textspansion/settings.java
@@ -105,7 +105,7 @@ public class settings extends PreferenceActivity
 			String authors = "Authors: Sean Barag and Vincent Tran";
 			String testers = "QA Tester: Nee Taylor";
 			String website = "Visit our website: http://1393Designs.com";
-			String cp = "\u00A92011 1393 Designatum, All Rights Reserved.";
+			String cp = "\u00A92011 1393 Designs, All Rights Reserved.";
 
 			message.setPadding(10, 10, 10, 10);
 			message.setText(versionString + "\n\n" + authors + "\n\n" + testers + "\n\n" + website + "\n\n" +	cp);
