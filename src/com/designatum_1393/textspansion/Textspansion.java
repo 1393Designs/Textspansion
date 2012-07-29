2d21a5a27baa39d557af2d3c886101ab1ebece32
Sean Barag <sjbarag@gmail.com>
2012-07-29 10:08:49 -0400

    Add basic "Hello World" activity
    Yes, seriously.

---
+++ src/com/designatum_1393/textspansion/Textspansion.java
@@ -0,0 +1,17 @@
+package com.designatum_1393.textspansion;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Textspansion extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.fragment_layout);
+	}
+}
+
