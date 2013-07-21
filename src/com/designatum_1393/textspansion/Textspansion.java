87d02c7c6cfbe1239e0b027792f9628a6714c8b9
Vince Tran <hello@vincetran.me>
2013-07-20 17:21:39 -0700

    I think I put the Notification Shade pref in a better spot
    The original place I had it was causing rare FCs because (I think)
    getActivity was returning null. I haven't gotten any FCs with the new
    code yet.

--- src/com/designatum_1393/textspansion/Textspansion.java
+++ src/com/designatum_1393/textspansion/Textspansion.java
@@ -1,14 +1,77 @@
 package com.designatum_1393.textspansion;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 public class Textspansion extends Activity
 {
+    private SharedPreferences sharedPreferences;
+    private NotificationManager mNotificationManager;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        SharedPreferences.OnSharedPreferenceChangeListener listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+                if (key.equals(Preferences.PREF_LOAD_ON_BOOT)) {
+                    if (prefs.getBoolean(key, false)) {
+                        Intent notificationIntent = new Intent(getApplicationContext(), Textspansion.class);
+                        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                        PendingIntent contentIntent = PendingIntent.getActivity(getApplicationContext(), 0, notificationIntent, 0);
+
+                        Notification.Builder builder = new Notification.Builder(getApplicationContext());
+                        builder.setContentTitle(getApplicationContext().getString(R.string.app_name));
+                        builder.setContentText(getApplicationContext().getString(R.string.notification_click));
+                        builder.setTicker(getApplicationContext().getString(R.string.notification_running));
+                        builder.setSmallIcon(R.drawable.textspansion_notification);
+                        builder.setOngoing(true);
+                        builder.setContentIntent(contentIntent);
+                        Notification notification = builder.getNotification();
+
+                        mNotificationManager.notify(1, notification);
+                    } else {
+                        mNotificationManager.cancelAll();
+                    }
+                }
+            }
+        };
+        sharedPreferences.registerOnSharedPreferenceChangeListener(listener);
 	}
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(sharedPreferences.getBoolean("notification", false)) {
+            Intent notificationIntent = new Intent(this, Textspansion.class);
+            notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+            Notification.Builder builder = new Notification.Builder(this);
+            builder.setContentTitle(getString(R.string.app_name));
+            builder.setContentText(getString(R.string.notification_click));
+            builder.setTicker(getString(R.string.notification_running));
+            builder.setSmallIcon(R.drawable.textspansion_notification);
+            builder.setOngoing(true);
+            builder.setContentIntent(contentIntent);
+            Notification notification = builder.getNotification();
+
+            mNotificationManager.notify(1, notification);
+        }
+        else {
+            mNotificationManager.cancelAll();
+        }
+    }
 }
--- src/com/designatum_1393/textspansion/fragments/ClipFragment.java
+++ src/com/designatum_1393/textspansion/fragments/ClipFragment.java
@@ -2,21 +2,14 @@ package com.designatum_1393.textspansion.fragments;
 
 import android.app.Dialog;
 import android.app.ListFragment;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.CheckBoxPreference;
-import android.preference.PreferenceManager;
 import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
 import android.view.ActionMode;
-import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -32,7 +25,6 @@ import android.widget.Toast;
 import com.designatum_1393.textspansion.Preferences;
 import com.designatum_1393.textspansion.R;
 import com.designatum_1393.textspansion.Sub;
-import com.designatum_1393.textspansion.Textspansion;
 import com.designatum_1393.textspansion.utils.SubsArrayAdapter;
 import com.designatum_1393.textspansion.utils.SubsDataSource;
 
@@ -50,36 +42,6 @@ public class ClipFragment extends ListFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        final NotificationManager mNotificationManager = (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-        SharedPreferences.OnSharedPreferenceChangeListener listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
-            public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
-                if (key.equals(Preferences.PREF_LOAD_ON_BOOT)) {
-                    if (prefs.getBoolean(key, false)) {
-                        Intent notificationIntent = new Intent(getActivity(), Textspansion.class);
-                        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                        PendingIntent contentIntent = PendingIntent.getActivity(getActivity(), 0, notificationIntent, 0);
-
-                        Notification.Builder builder = new Notification.Builder(getActivity());
-                        builder.setContentTitle(getActivity().getString(R.string.app_name));
-                        builder.setContentText(getActivity().getString(R.string.notification_click));
-                        builder.setTicker(getActivity().getString(R.string.notification_running));
-                        builder.setSmallIcon(R.drawable.textspansion_notification);
-                        builder.setOngoing(true);
-                        builder.setContentIntent(contentIntent);
-                        Notification notification = builder.getNotification();
-
-
-                        mNotificationManager.notify(1, notification);
-                    } else {
-                        mNotificationManager.cancelAll();
-                    }
-                }
-            }
-        };
-        sharedPreferences.registerOnSharedPreferenceChangeListener(listener);
-
     }
 
     @Override
@@ -108,6 +70,11 @@ public class ClipFragment extends ListFragment {
                 return true;
             }
         });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
     }
 
     private ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {
