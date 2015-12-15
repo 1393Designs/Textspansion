a79ab208262280cf38baae46c5839fcc34333192
Sean Barag <sjbarag@gmail.com>
2015-12-14 21:58:52 -0800

    Add documentation to ExpansionAdapter and EA.ViewHolder

--- app/src/main/java/org/barag/textspansion/ExpansionAdapter.java
+++ app/src/main/java/org/barag/textspansion/ExpansionAdapter.java
@@ -19,34 +19,52 @@ import butterknife.ButterKnife;
  */
 public class ExpansionAdapter extends RecyclerView.Adapter {
 
+    /** The set of all {@link org.barag.textspansion.domain.Expansion}s to be displayed. */
     private List<Expansion> dataset;
 
+    /**
+     * A simple implementation of {@link android.support.v7.widget.RecyclerView.ViewHolder}, copied mostly from
+     * http://developer.android.com/training/material/lists-cards.html#RecyclerView.
+     */
     private static class ViewHolder extends RecyclerView.ViewHolder {
+        /** The view to be held by the ViewHolder.  In this case, the parent view for a single Expansion. */
         private LinearLayout heldView;
 
-        public ViewHolder(LinearLayout layout) {
+        /**
+         * Creates a new ViewHolder.
+         * @param layout the parent view for a single Expansion.
+         */
+        public ViewHolder(final LinearLayout layout) {
             super(layout);
             this.heldView = layout;
         }
 
+        /**
+         * Returns the held view that acts as the parent for a single Expansion element.
+         * @return the held view.
+         */
         public LinearLayout getHeldView() {
             return heldView;
         }
     }
 
-    public ExpansionAdapter(List<Expansion> seedData) {
+    /**
+     * Creates an adapter to a seed list of {@link Expansion} elements.
+     * @param seedData a starting list of {@link Expansion}s to contain.
+     */
+    public ExpansionAdapter(final List<Expansion> seedData) {
         this.dataset = seedData;
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.expansion_row, parent, false);
         return new ViewHolder((LinearLayout) view);
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
         Expansion modelToDisplay = this.dataset.get(position);
 
         LinearLayout layout = ((ViewHolder) holder).getHeldView();
